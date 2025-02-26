9b134648278b6c516060e8e396a43a3cd00d991e
nadrahoda <nadrahoda2412@gmail.com>
2025-02-26 10:45:57 +0530

    fixed navar type props error

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,18 @@
 import Navbar from '@/components/Navbar'
-import React from 'react'
+import React, {useState} from 'react'
 
 import AboutUs from '../../components/AboutUs'
 import Team from '@/components/Team'
 import Footer from '@/components/Footer'
 import Hiring from '@/components/Hiring'
 import OurPartners from '@/components/OurPartners'
+
 const page = () => {
+  const [selectedOption, setSelectedOption] =
+    useState<string>('Strangers Unite')
   return (
     <>
-      <Navbar/>
+      <Navbar selectedOption={selectedOption} setSelectedOption={setSelectedOption}/>
       <AboutUs/>
       <Team/>
       <Hiring/>
